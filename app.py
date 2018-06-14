9d47386d719fa05f69fb39925d87330946ce5981
Om Patel <om208patel@gmail.com>
2018-06-14 11:54:04 +0530

    host removed

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def delete_task(product_id):
 
 #command to run the API on the given host and port
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, port=8080)
